7a44da2ff4796d5825e1f2a5eebd31a8e2cd9fb5
Hugo Garcia <hugoallangm@gmail.com>
2015-10-22 09:38:48 -0600

    Agregando otros y otras a lista de ignorados

--- R/envi.R
+++ R/envi.R
@@ -4,7 +4,7 @@ pkg.env$color1 <- rgb(0,0,0, maxColorValue = 255) #0 0 0
 pkg.env$color2 <- rgb(0.5,0.5,0.5)
 pkg.env$colorRelleno <- rgb(1,1,1) # 1 1 1 
 pkg.env$repu <- c("Total República, Total republica, Total república, Total Republica")
-pkg.env$ignorado <- c("Ignorado", "ignorado", "IGNORADO", "Ignorada", "ignorada")
+pkg.env$ignorado <- c("Ignorado", "ignorado", "IGNORADO", "Ignorada", "ignorada", "otro", "otros", "otra", "otras", "Otras", "Otros")
 pkg.env$exclusion <- c(pkg.env$ignorado, "otro", "otros", "otra", "otras", "Otras", "Otros")
 pkg.env$gris <- rgb(200,200,200, maxColorValue = 255)
 pkg.env$grisBase <- rgb(152,152,152, maxColorValue = 255)
